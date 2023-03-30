c061b24f03b1f01a5478f08b0770ec8a3e8be185
Miguel Rietz <miguel.rietz@netcentric.biz>
2023-03-30 19:57:01 +0200

    initial commit

---
+++ TurkeyGateway/src/main/java/devs/mrp/springturkey/TurkeyGatewayApplication.java
@@ -0,0 +1,13 @@
+package devs.mrp.springturkey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurkeyGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurkeyGatewayApplication.class, args);
+	}
+
+}
---
+++ TurkeyGateway/src/test/java/devs/mrp/springturkey/TurkeyGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package devs.mrp.springturkey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurkeyGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
